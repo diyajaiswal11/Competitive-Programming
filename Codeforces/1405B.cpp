f41704adbf14da0ab6f54de5c3a8028b3b77f99e
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-09-10 02:51:57 +0530

    Day 0

---
+++ Codeforces/1405B.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define printclock cerr<<"Time : "<<1000*(ld)clock()/(ld)CLOCKS_PER_SEC<<"ms\n"
+#define MAX 1000000
+#define mod 1000000007
+#define ll long long int
+#define ld long double
+#define ull unsigned long long
+#define li long int
+#define str string
+#define fr(i,n) for(ll i = 0; i<n; i++)
+#define frj(j,i,n) for(ll j = i; j<n; j++)
+#define frev(i,n) for(ll i = n-1; i>=0; i--)
+#define all(x) x.begin(),x.end()
+int main() {
+    ll t;
+    cin>>t;
+    fr(i,t)
+    {
+        ll n,ans=0;
+        cin>>n;
+        vector<ll> a(n);
+        fr(j,n)
+        cin>>a[j];
+        fr(j,n)
+        {
+            ans+=a[j];
+            if(ans<0)
+            ans=0;
+        }
+        cout<<ans<<"\n";
+    }
+	return 0;
+}
